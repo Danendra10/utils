934ef9109930305db727256f482f8d45f9f42860
Danendra Clevero Ananda <danendraclever24@gmail.com>
2022-10-12 20:19:10 +0700

    update role switching

--- src/utils.cpp
+++ src/utils.cpp
@@ -105,60 +105,66 @@ ObstacleDetection ObstacleCheck(float theta, float theta_thresh, uint16_t dist)
         {
             /* Check if the obstacle is in the range of our dist thresh */
             /**
-             * Got some error in this line of code, 
-             * TODO: try to use try and catch to debug 
-            */
-            if (obs_on_field[i] > dist && i != final_index)
+             * Got some error in this line of code,
+             * TODO: try to use try and catch to debug
+             */
+            try
             {
-                printf("helo");
-                obs_data.status = 1;
-                obs_data.angle = i * 6;
-                obs_data.distance = obs_on_field[i];
-                obs_data.pos_x = pos_robot[0] + obs_on_field[i] * cos((i * 6 * DEG2RAD));
-                obs_data.pos_y = pos_robot[1] + obs_on_field[i] * sin((i * 6 * DEG2RAD));
-
-                obs_data.detection[i].status = 1;
-                obs_data.detection[i].pos_x = obs_data.pos_x;
-                obs_data.detection[i].pos_y = obs_data.pos_y;
-
-                obs_counter++;
-
-                if (obs_counter == 1)
-                    obs_start = i;
-
-                printf("All datas: %d %f %f %f %f\n", obs_data.status, obs_data.angle, obs_data.distance, obs_data.pos_x, obs_data.pos_y);
-            }
-            else
-            {
-                if (obs_counter >= 2)
+                if (obs_on_field.at(i) < dist && i != final_index)
                 {
-                    obs_final = i - 1;
                     obs_data.status = 1;
-                    obs_data.angle = (obs_start + obs_final) * 6;
+                    obs_data.angle = i * 6;
+                    obs_data.distance = obs_on_field[i];
+                    obs_data.pos_x = pos_robot[0] + obs_on_field[i] * cos((i * 6 * DEG2RAD));
+                    obs_data.pos_y = pos_robot[1] + obs_on_field[i] * sin((i * 6 * DEG2RAD));
 
-                    obs_data.pos_x = 0;
-                    obs_data.pos_y = 0;
+                    obs_data.detection[i].status = 1;
+                    obs_data.detection[i].pos_x = obs_data.pos_x;
+                    obs_data.detection[i].pos_y = obs_data.pos_y;
 
-                    for (uint8_t j = obs_start; j <= obs_final; j++)
-                    {
-                        obs_data.pos_x += obs_data.detection[j].pos_x;
-                        obs_data.pos_y += obs_data.detection[j].pos_y;
-                    }
+                    obs_counter++;
 
-                    obs_data.pos_x /= (obs_final - obs_start);
-                    obs_data.pos_y /= (obs_final - obs_start);
+                    if (obs_counter == 1)
+                        obs_start = i;
 
-                    obs_data.pos_x += 25 * cosf(obs_data.angle * M_PI * 0.005555556);
-                    obs_data.pos_y += 25 * sinf(obs_data.angle * M_PI * 0.005555556);
-
-                    float buffer_obs_dist;
-
-                    buffer_obs_dist = Pythagoras(pos_robot[0], pos_robot[1], obs_data.pos_x, obs_data.pos_y);
-
-                    obs_dist.push_back(buffer_obs_dist);
-                    obs_index.push_back((int)(obs_start + obs_final) * 0.5);
+                    printf("All datas: %d %f %f %f %f\n", obs_data.status, obs_data.angle, obs_data.distance, obs_data.pos_x, obs_data.pos_y);
                 }
-                obs_counter = 0;
+                else
+                {
+                    if (obs_counter >= 2)
+                    {
+                        obs_final = i - 1;
+                        obs_data.status = 1;
+                        obs_data.angle = (obs_start + obs_final) * 6;
+
+                        obs_data.pos_x = 0;
+                        obs_data.pos_y = 0;
+
+                        for (uint8_t j = obs_start; j <= obs_final; j++)
+                        {
+                            obs_data.pos_x += obs_data.detection[j].pos_x;
+                            obs_data.pos_y += obs_data.detection[j].pos_y;
+                        }
+
+                        obs_data.pos_x /= (obs_final - obs_start);
+                        obs_data.pos_y /= (obs_final - obs_start);
+
+                        obs_data.pos_x += 25 * cosf(obs_data.angle * M_PI * 0.005555556);
+                        obs_data.pos_y += 25 * sinf(obs_data.angle * M_PI * 0.005555556);
+
+                        float buffer_obs_dist;
+
+                        buffer_obs_dist = Pythagoras(pos_robot[0], pos_robot[1], obs_data.pos_x, obs_data.pos_y);
+
+                        obs_dist.push_back(buffer_obs_dist);
+                        obs_index.push_back((int)(obs_start + obs_final) * 0.5);
+                    }
+                    obs_counter = 0;
+                }
+            }
+            catch (const std::out_of_range &oor)
+            {
+                std::cerr << "Out of Range error: " << oor.what() << '\n';
             }
         }
     }
